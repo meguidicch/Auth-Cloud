709eb1ffe2cfc2a9aff78edac795dbe1e60d1f9e
meguidicch <ameguidicch@gmail.com>
2023-01-09 09:09:39 +0100

    first commit

---
+++ Routes/User.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const { SignUp, SignIn } = require('../Controlles/User');
+const isAuth = require('../Middlewares/isAuth');
+const { validationSignUp, Validation, validationSignIn } = require('../Middlewares/Validator');
+const userRouter = express.Router()
+
+userRouter.post('/SignUp',validationSignUp,Validation,SignUp)
+ 
+userRouter.post('/SignIn',validationSignIn,Validation,SignIn)
+
+userRouter.get('/currentUser',isAuth,(req,res)=>{ res.send(req.user)})
+
+module.exports = userRouter
